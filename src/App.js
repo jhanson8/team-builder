05919d9b3667a33e9b57438d5c1576a4459d0b31
Jonathan Hanson <jhanson8@uoregon.edu>
2019-12-18 14:34:07 -0800

    mvp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+/* jshint esversion: 6 */
+import React, {useState} from 'react';
+import Members from "./components/members.js";
+import Form from "./components/form.js";
 import './App.css';
 
 function App() {
+  const [members, setMembers] = useState([
+    {
+      id: 1,
+      name: "Jonathan Hanson",
+      email:"jhanson@me.com",
+      role:"Full-Stack Engineer"
+    },
+    {
+      id: 2,
+      name: "Jon jones",
+      email:"jjones@me.com",
+      role:"Front-End Engineer"
+    }
+  ]);
+  const addNewBox = box => {
+  const newBox = {
+    id: Date.now(),
+    name: box.name,
+    email: box.email,
+    role: box.role
+  };
+  setMembers([...members, newBox]);
+};
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>The Team</h1>
+    <Form addNewBox={addNewBox} />
+    <Members members={members}/>
+
     </div>
   );
 }
